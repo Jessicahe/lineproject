394200fdc7d4c9445db6f1c40e0dab07626cb46f
Jessicahe <smilinginheart@gmail.com>
2016-08-16 22:35:02 +0800

    v3

--- main.go
+++ main.go
@@ -21,63 +21,24 @@ import (
 	"strings"
 	"net/url"
 	"io/ioutil"
+	"math/rand"
+	"time"
 
 	"github.com/JustinBeckwith/go-yelp/yelp"
+	"github.com/guregu/null"
 	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 var bot *linebot.Client
 var o *yelp.AuthOptions
-//shorten url
-const (
-	TINY_URL = 1
-	IS_GD    = 2
-)
 
 type UrlShortener struct {
 	ShortUrl    string
 	OriginalUrl string
 }
 
-func getResponseData(urlOrig string) string {
-	response, err := http.Get(urlOrig)
-	if err != nil {
-		fmt.Print(err)
-	}
-	defer response.Body.Close()
-	contents, err := ioutil.ReadAll(response.Body)
-	return string(contents)
-}
-
-func tinyUrlShortener(urlOrig string) (string, string) {
-	escapedUrl := url.QueryEscape(urlOrig)
-	tinyUrl := fmt.Sprintf("http://tinyurl.com/api-create.php?url=%s", escapedUrl)
-	return getResponseData(tinyUrl), urlOrig
-}
-
-func isGdShortener(urlOrig string) (string, string) {
-	escapedUrl := url.QueryEscape(urlOrig)
-	isGdUrl := fmt.Sprintf("http://is.gd/create.php?url=%s&format=simple", escapedUrl)
-	return getResponseData(isGdUrl), urlOrig
-}
-
-func (u *UrlShortener) short(urlOrig string, shortener int) *UrlShortener {
-	switch shortener {
-	case TINY_URL:
-		shortUrl, originalUrl := tinyUrlShortener(urlOrig)
-		u.ShortUrl = shortUrl
-		u.OriginalUrl = originalUrl
-		return u
-	case IS_GD:
-		shortUrl, originalUrl := isGdShortener(urlOrig)
-		u.ShortUrl = shortUrl
-		u.OriginalUrl = originalUrl
-		return u
-	}
-	return u
-}
-//surl
 func main() {
+	rand.Seed(time.Now().UnixNano())
 	strID := os.Getenv("ChannelID")
 	numID, err := strconv.ParseInt(strID, 10, 64)
 	if err != nil {
@@ -85,7 +46,9 @@ func main() {
 	}
 
 	bot, err = linebot.NewClient(numID, os.Getenv("ChannelSecret"), os.Getenv("MID"))
-	log.Println("Bot:", bot, " err:", err)
+	if err != nil {
+		log.Fatal("Wrong environment setting about ChannelSecret and MID")
+	}
 
 	// check environment variables
 	o = &yelp.AuthOptions{
@@ -96,13 +59,36 @@ func main() {
 	}
 
 	if o.ConsumerKey == "" || o.ConsumerSecret == "" || o.AccessToken == "" || o.AccessTokenSecret == "" {
-		log.Println("Wrong environment setting about yelp-api-keys")
+		log.Fatal("Wrong environment setting about yelp-api-keys")
 	}
 
 	http.HandleFunc("/callback", callbackHandler)
 	port := os.Getenv("PORT")
 	addr := fmt.Sprintf(":%s", port)
 	http.ListenAndServe(addr, nil)
+}
+
+func getResponseData(urlOrig string) string {
+	response, err := http.Get(urlOrig)
+	if err != nil {
+		log.Println(err)
+	}
+	defer response.Body.Close()
+	contents, err := ioutil.ReadAll(response.Body)
+	return string(contents)
+}
+
+func isGdShortener(urlOrig string) (string, string) {
+	escapedUrl := url.QueryEscape(urlOrig)
+	isGdUrl := fmt.Sprintf("http://is.gd/create.php?url=%s&format=simple", escapedUrl)
+	return getResponseData(isGdUrl), urlOrig
+}
+
+func (u *UrlShortener) short(urlOrig string) *UrlShortener {
+	shortUrl, originalUrl := isGdShortener(urlOrig)
+	u.ShortUrl = shortUrl
+	u.OriginalUrl = originalUrl
+	return u
 }
 
 func callbackHandler(w http.ResponseWriter, r *http.Request) {
@@ -115,55 +101,109 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-
 	for _, result := range received.Results {
 		content := result.Content()
 
+		//identify different ContentType
 		if content != nil && content.IsOperation && content.OpType == 4 {
-			_, err := bot.SendText([]string{result.RawContent.Params[0]}, "Hi~\n歡迎加入 Delicious!\n如果想查詢附近或各地美食都可以LINE我呦！\n下列3種輸入方式：\n1.地區\nex：台北市信義區\n\n2.食物 地區\nex：義大利麵 新北市新莊區\n\n3.傳送位置訊息")
-			//_, err = bot.SendSticker([]string{result.RawContent.Params[0]}, 11, 1, 100)
+			//add new friend
+			_, err := bot.SendText([]string{result.RawContent.Params[0]}, "Hi~\n歡迎加入 Delicious!\n如果想查詢附近或各地美食都可以LINE我呦！\n\n有下列3種輸入方式：\n1.地區\nex：台北市信義區\n\n2.食物 地區\nex：義大利麵 新北市新莊區\n\n3.傳送位置訊息")
 			if err != nil {
-				log.Println("New friend add event.")
+				log.Println(err)
 			}
-		}
-
-		if content != nil && content.IsMessage && content.ContentType == linebot.ContentTypeText {
-			text, err := content.TextContent()
-			c := strings.Split(text.Text, " ")
+		} else if content != nil && content.ContentType == linebot.ContentTypeLocation {
+			//receive location
 			// create a new yelp client with the auth keys
 			client := yelp.New(o, nil)
-			if len(c) == 2 {
-				// make a simple query
-				results, err := client.DoSimpleSearch(c[0], c[1])
+
+			loc, err := content.LocationContent()
+			if err != nil {
+				log.Println(err)
+			}
+
+			// Build an advanced set of search criteria that include
+			// general options, and coordinate options.
+			s := yelp.SearchOptions{
+				GeneralOptions: &yelp.GeneralOptions{
+				    Term: "food,restaurants",
+				},
+				CoordinateOptions: &yelp.CoordinateOptions{
+					Latitude:  null.FloatFrom(loc.Latitude),
+					Longitude: null.FloatFrom(loc.Longitude),
+				},
+			}
+
+			// Perform the search using the search options
+			results, err := client.DoSearch(s)
+			if err != nil {
+				log.Println(err)
+				_, err = bot.SendText([]string{content.From}, "查無資料！\n\n請輸入：\n1.地區\nex：台北市信義區\n\n2.食物 地區\nex：義大利麵 新北市新莊區\n\n3.傳送位置訊息")
+			}
+
+			for j := 0; j < 3; j++ {
+				i := rand.Intn(20)
+				urlOrig := UrlShortener{}
+				urlOrig.short(results.Businesses[i].MobileURL)
+				address := strings.Join(results.Businesses[i].Location.DisplayAddress, ",")
+				var largeImageURL = strings.Replace(results.Businesses[i].ImageURL, "ms.jpg", "l.jpg", 1)
+
+				_, err = bot.SendImage([]string{content.From}, largeImageURL, largeImageURL)
+				_, err = bot.SendText([]string{content.From}, "店名："+results.Businesses[i].Name+"\n電話："+results.Businesses[i].Phone+"\n評比："+strconv.FormatFloat(float64(results.Businesses[i].Rating), 'f', 1, 64)+"\n更多資訊：" + urlOrig.ShortUrl)
+				_, err = bot.SendLocation([]string{content.From}, results.Businesses[i].Name+"\n", address, float64(results.Businesses[i].Location.Coordinate.Latitude), float64(results.Businesses[i].Location.Coordinate.Longitude))
+			}
+		} else if content != nil && content.IsMessage && content.ContentType == linebot.ContentTypeText {
+			//receive text
+			// create a new yelp client with the auth keys
+			client := yelp.New(o, nil)
+
+			text, err := content.TextContent()
+			c := strings.Split(text.Text, " ")
+
+			if len(c) == 1 {
+				// make a simple query for location
+				results, err := client.DoSimpleSearch("food,restaurants", c[0])
 				if err != nil {
-					_, err = bot.SendText([]string{content.From}, "查無資料！\n\n請輸入：\n1.地區\nex：台北市信義區\n\n2.食物 地區\nex：義大利麵 新北市新莊區\n\n3.傳送位置訊息")
 					log.Println(err)
+					_, err = bot.SendText([]string{content.From}, "查無資料！\n\n請輸入：\n1.地區\nex：台北市信義區\n\n2.食物 地區\nex：義大利麵 新北市新莊區\n\n3.傳送位置訊息")
 				}
 
-				for i := 0; i < 3; i++ {
-					weburl := results.Businesses[i].URL
+				for j := 0; j < 3; j++ {
+					i := rand.Intn(20)
 					urlOrig := UrlShortener{}
-					urlOrig.short(weburl, IS_GD)
+					urlOrig.short(results.Businesses[i].MobileURL)
 					address := strings.Join(results.Businesses[i].Location.DisplayAddress, ",")
 					var largeImageURL = strings.Replace(results.Businesses[i].ImageURL, "ms.jpg", "l.jpg", 1)
-					_, err = bot.SendImage([]string{content.From}, results.Businesses[i].MobileURL, largeImageURL)
-					_, err = bot.SendText([]string{content.From}, "店名："+results.Businesses[i].Name+"\n電話："+results.Businesses[i].Phone+"\n評比："+strconv.FormatFloat(float64(results.Businesses[i].Rating), 'f', 1, 64)+"\n更多資訊：\n" + urlOrig.ShortUrl)
-					_, err = bot.SendLocation([]string{content.From},"地址：", address, float64(results.Businesses[i].Location.Coordinate.Latitude), float64(results.Businesses[i].Location.Coordinate.Longitude))
-					//_, err = bot.SendText([]string{content.From}, "更多資訊: \n" + urlOrig.ShortUrl)
+
+					_, err = bot.SendImage([]string{content.From}, largeImageURL, largeImageURL)
+					_, err = bot.SendText([]string{content.From}, "店名："+results.Businesses[i].Name+"\n電話："+results.Businesses[i].Phone+"\n評比："+strconv.FormatFloat(float64(results.Businesses[i].Rating), 'f', 1, 64)+"\n更多資訊：" + urlOrig.ShortUrl)
+					_, err = bot.SendLocation([]string{content.From}, results.Businesses[i].Name+"\n", address, float64(results.Businesses[i].Location.Coordinate.Latitude), float64(results.Businesses[i].Location.Coordinate.Longitude))
+				}
+			} else if len(c) == 2 {
+				// make a simple query for food and location
+				results, err := client.DoSimpleSearch(c[0], c[1])
+				if err != nil {
+					log.Println(err)
+					_, err = bot.SendText([]string{content.From}, "查無資料！\n\n請輸入：\n1.地區\nex：台北市信義區\n\n2.食物 地區\nex：義大利麵 新北市新莊區\n\n3.傳送位置訊息")
+				}
+
+				for j := 0; j < 3; j++ {
+					i := rand.Intn(20)
+					urlOrig := UrlShortener{}
+					urlOrig.short(results.Businesses[i].MobileURL)
+					address := strings.Join(results.Businesses[i].Location.DisplayAddress, ",")
+					var largeImageURL = strings.Replace(results.Businesses[i].ImageURL, "ms.jpg", "l.jpg", 1)
+
+					_, err = bot.SendImage([]string{content.From}, largeImageURL, largeImageURL)
+					_, err = bot.SendText([]string{content.From}, "店名："+results.Businesses[i].Name+"\n電話："+results.Businesses[i].Phone+"\n評比："+strconv.FormatFloat(float64(results.Businesses[i].Rating), 'f', 1, 64)+"\n更多資訊：" + urlOrig.ShortUrl)
+					_, err = bot.SendLocation([]string{content.From}, results.Businesses[i].Name+"\n", address, float64(results.Businesses[i].Location.Coordinate.Latitude), float64(results.Businesses[i].Location.Coordinate.Longitude))
 				}
 			} else {
 				_, err = bot.NewMultipleMessage().
-					AddText("輸入格式錯誤！\n\n請輸入：\n1.地區\nex：台北市信義區\n\n2.食物 地區\nex：義大利麵 新北市新莊區\n\n3.傳送位置訊息").
-					//AddSticker(1, 1, 100).
+					AddText("格式輸入錯誤！\n\n請輸入：\n1.地區\nex：台北市信義區\n\n2.食物 地區\nex：義大利麵 新北市新莊區\n\n3.傳送位置訊息").
 					Send([]string{content.From})
 			}
 			if err != nil {
-				log.Println("OK")
-			}
-			//_, err = bot.SendText([]string{content.From}, "查無資料！\n\n請輸入：\n1.地區\nex：台北市信義區\n\n2.食物 地區\nex：義大利麵 新北市新莊區\n\n3.傳送位置訊息")
-			//_, err = bot.SendSticker([]string{content.From}, 11, 1, 100)
-			if err != nil {
-				log.Println("wait for new message")
+				log.Println(err)
 			}
 		}
 	}
